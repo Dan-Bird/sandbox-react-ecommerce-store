156fd730b60820bf85aa54f8b6f6b1e202be5d57
Daniel <info@daniel-bird.co.uk>
2021-01-01 15:36:49 +0000

    Introduce Redux with userReducer

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
@@ -8,9 +9,11 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,19 @@
+import { actionType } from '../actionTypes';
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case actionType.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
