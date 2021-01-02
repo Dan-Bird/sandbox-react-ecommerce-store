060653f65a0f022f2a32c731e48ca13ae8812b3a
Daniel <info@daniel-bird.co.uk>
2021-01-02 15:10:37 +0000

    Enhance custom button

--- src/components/CustomButton/CustomButton.jsx
+++ src/components/CustomButton/CustomButton.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import styles from './CustomButton.module.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => {
   return (
     <button
-      className={`${styles['custom-button']} ${
-        isGoogleSignIn && styles['custom-button--google-sign-in']
-      }`}
+      className={`
+          ${styles['custom-button']}
+          ${isGoogleSignIn ? styles['custom-button--google-sign-in'] : ''}
+          ${inverted ? styles['custom-button--inverted'] : ''}
+        `}
       {...otherProps}
     >
       {children}
