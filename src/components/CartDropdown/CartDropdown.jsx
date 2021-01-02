8a702c4a87f459fefcbfc7b4115bfefb1c413d22
Daniel <info@daniel-bird.co.uk>
2021-01-02 19:09:15 +0000

    Memoize rendering of components so they do not render whenever the global state is changed

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 import CartItem from '../CartItem/CartItem';
 import CustomButton from '../CustomButton/CustomButton';
 import styles from './CartDropdown.module.scss';
 
 const CartDropdown = ({ cartItems }) => {
+  console.log('renderign');
+
   return (
     <div className={styles['cart-dropdown']}>
       <div className={styles['cart-items']}>
@@ -17,8 +20,8 @@ const CartDropdown = ({ cartItems }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import styles from './CartIcon.module.scss';
 
@@ -13,11 +14,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  itemCount: cartItems.reduce(
-    (accumulated, currentItem) => accumulated + currentItem.quantity,
-    0
-  ),
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state),
 });
 
 const mapDispatchToProps = dispatch => ({
