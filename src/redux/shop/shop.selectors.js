182fdc76803c284e65e87c0a77659c26bb674e51
Daniel <info@daniel-bird.co.uk>
2021-01-03 19:05:06 +0000

    Memoize selector call

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,4 +1,5 @@
 import { createSelector } from 'reselect';
+import memoize from 'lodash.memoize';
 
 const COLLECTION_ID_MAP = {
   hats: 1,
@@ -15,9 +16,10 @@ export const selectShopCollections = createSelector(
   shop => shop.collections
 );
 
-export const selectCollection = collectionUrlParam =>
+export const selectCollection = memoize(collectionUrlParam =>
   createSelector([selectShopCollections], collections =>
     collections.find(
       collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
     )
-  );
+  )
+);
