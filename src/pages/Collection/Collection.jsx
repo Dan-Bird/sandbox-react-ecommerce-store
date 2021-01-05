fa399045dd99f812417db29a62b27ab7f76f58e1
Daniel <info@daniel-bird.co.uk>
2021-01-05 17:54:45 +0000

    Improve collections pages

--- src/pages/Collection/Collection.jsx
+++ src/pages/Collection/Collection.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionItem from '../../components/CollectionItem/CollectionItem';
+import styles from './Collection.module.scss';
 
 const Collection = ({ collection }) => {
-  console.log(`collection: `, collection);
+  const { title, items } = collection;
 
   return (
-    <div>
-      <h2>Collection page</h2>
-    </div>
+    <section className={styles['collection-page']}>
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.items}>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </section>
   );
 };
 
