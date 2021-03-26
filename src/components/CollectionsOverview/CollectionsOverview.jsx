86f787cedf626f72a1bb7d91a49a0cfd73008c7e
Daniel <info@daniel-bird.co.uk>
2021-03-26 19:05:54 +0000

    Convert CollectionsOverview to context

--- src/components/CollectionsOverview/CollectionsOverview.jsx
+++ src/components/CollectionsOverview/CollectionsOverview.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCollectionForPreview } from '../../redux/shop/shop.selectors';
+import React, { useContext } from 'react';
+import CollectionsContext from '../../contexts/collections/collections-context';
 import CollectionPreview from '../CollectionPreview/CollectionPreview';
 import styles from './CollectionsOverview.module.scss';
 
-const CollectionsOverview = ({ collections }) => {
+const CollectionsOverview = () => {
+  const collectionsMap = useContext(CollectionsContext);
+  const collections = Object.keys(collectionsMap).map(
+    key => collectionsMap[key]
+  );
+
   return (
     <div className={styles['collections-overview']}>
       {collections.map(({ id, ...otherCollectionProps }) => (
@@ -15,8 +18,4 @@ const CollectionsOverview = ({ collections }) => {
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionForPreview,
-});
-
-export default connect(mapStateToProps)(CollectionsOverview);
+export default CollectionsOverview;
