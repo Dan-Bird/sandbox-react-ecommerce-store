6e409ca6f47fa768669c4a0c9347ed3e8e6665b1
Daniel <info@daniel-bird.co.uk>
2021-04-05 10:54:56 +0100

    Remove unused import

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import { PersistGate } from 'redux-persist/es/integration/react';
 import { ApolloProvider } from 'react-apollo';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloClient, gql } from 'apollo-boost';
+import { ApolloClient } from 'apollo-boost';
 import './index.css';
 import App from './App';
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
